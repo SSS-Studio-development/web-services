96a0ac9e0869e192e0b24a8e763910088ea60168
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-06-10 11:11:14 +0530

    date conversion API added

--- code/app/views.py
+++ code/app/views.py
@@ -2,10 +2,12 @@ from app import app
 from flask import request
 import requests as req
 import bs4
+from dateutil.parser import parse
+from datetime import datetime
 
 @app.route("/translate", methods=['POST'])
 def trans():
-    
+
     sentence = request.json['sentence']
     words = sentence.split()
     answer = []
@@ -19,3 +21,19 @@ def trans():
             answer.append(word)
 
     return " ".join(answer)
+
+@app.route("/date-translate", methods=['POST'])
+def datetrans():
+    try :
+        date_str = request.json['date']
+    except :
+        return 'Error occured, date format not valid or date not passed'
+    try :
+        forma = request.json['format']
+    except:
+        return 'format not provided'
+    try:
+        date = parse(date_str)
+    except :
+        return 'new date format not valid'
+    return datetime.strftime(date,forma)
