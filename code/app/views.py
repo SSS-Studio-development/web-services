b1dc646e7c49cc00d11c565a8230ebee4baab348
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-06-10 09:28:05 +0530

    test cases updated

--- code/app/views.py
+++ code/app/views.py
@@ -5,16 +5,11 @@ import bs4
 
 @app.route("/translate", methods=['POST'])
 def trans():
-
-    print request.json
+    
     sentence = request.json['sentence']
-
-    print 'raj'
-    print sentence
     words = sentence.split()
     answer = []
     for word in words :
-        print word
         res = req.get('http://www.phonemicchart.com/transcribe/?w='+word)
         soup = bs4.BeautifulSoup(res.text)
         ret = soup.center
--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,11 @@
 #! tests for the rest APIs 
 
 echo "requesting for phoneticd translation of word 'between hello word' :"
-curl -H "Content-Type: application/json" -X GET -d '{"sentence":"between hello world"}' http://localhost:5000/translate
+curl -H "Content-Type: application/json" -X POST -d '{"sentence":"between hello world and we are stuck between here and there for the good of the world"}' http://localhost:5000/translate
 echo
 echo "response received"
 
 echo "requesting for phoneticd translation of word 'excited' :"
-curl -H "Content-Type: application/json" -X GET -d '{"sentence":"excited"}' http://localhost:5000/translate
+curl -H "Content-Type: application/json" -X POST -d '{"sentence":"excited"}' http://localhost:5000/translate
 echo 
 echo "response received"
