0756e78e3b6854c392caeeb358ca19b0e64f8da9
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-06-14 09:55:34 +0530

    CORS library added for http access

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -1,7 +1,34 @@
 from flask import Flask
 from flask import request
-from flask.ext.cors import CORS
+from flask_cors import CORS
+from flask import request
+import requests as req
+import bs4
+from dateutil.parser import parse
+from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-from app import views
+
+@app.route("/translate", methods=['POST'])
+def trans():
+
+    sentence = request.json['sentence']
+    words = sentence.split()
+    answer = []
+    for word in words :
+        res = req.get('http://www.phonemicchart.com/transcribe/?w='+word)
+        soup = bs4.BeautifulSoup(res.text)
+        ret = soup.center
+        if ret:
+            answer.append(ret.string)
+        else :
+            answer.append(word)
+
+            return " ".join(answer)
+
+
+@app.route("/test", methods=['GET'])
+def test():
+    return "some test string"
