3bf411ba483abc40017b7f9cf658a8f6775160c5
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-06-16 23:22:54 +0530

    word list added

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -4,12 +4,15 @@ from flask_cors import CORS
 from flask import request
 import requests as req
 import bs4
+import pickle
 from dateutil.parser import parse
 from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+word_list = pickle.load( open( "word_list.p", "rb" ) )
 
 @app.route("/translate", methods=['POST'])
 def trans():
@@ -18,6 +21,25 @@ def trans():
     words = sentence.split()
     answer = []
     for word in words :
+
+        word = word.lower()
+        if word in word_list:
+            answer.append(word_list[word])
+        else :
+            answer.append(word)
+
+
+    return " ".join(answer)
+
+@app.route("/translatelive", methods=['POST'])
+def translive():
+
+    sentence = request.json['sentence']
+    words = sentence.split()
+    answer = []
+    for word in words :
+
+        word = word.lower()
         res = req.get('http://www.phonemicchart.com/transcribe/?w='+word)
         soup = bs4.BeautifulSoup(res.text)
         ret = soup.center
